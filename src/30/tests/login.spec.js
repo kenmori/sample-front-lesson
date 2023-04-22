2259524b8083527739802a49a822b668c251acc3
kenmori <kenjimoritata@gmail.com>
2023-04-22 22:06:53 +0900

    fix: test multipul

--- src/30/tests/login.spec.js
+++ src/30/tests/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('have text', async ({ page }) => {
-  await page.goto('http://localhost:3000/login.html');
+  await page.goto('http://localhost:3000/30/login.html');
   await page.getByRole('link', { name: 'Sign Up' }).click();
   await expect(page).toHaveTitle(/Sign Up/)
   
