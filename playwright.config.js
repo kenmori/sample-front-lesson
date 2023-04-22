820c70304e920a79adc76743607dd503836f0eff
kenmori <kenjimoritata@gmail.com>
2023-04-22 22:20:45 +0900

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -70,10 +70,10 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'yarn dev',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
