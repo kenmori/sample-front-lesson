929272d0caa5b0807c548a7012a61f903a50ed0a
Haruna Takeda <h136479442@gmail.com>
2023-04-29 10:30:16 +0900

    fix: Improvements to make the BUILD work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
+const { resolve } = require('path');
+
+const root = 'src';
+
 export default defineConfig({
-  root: './src',
+  root: root,
   build: {
-    base: '/',
     outDir: '../dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          }
+          if(extType === 'css') {
+            return `assets/css/content.css`;
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: `assets/js/[name].js`,
+        entryFileNames: `assets/js/[name].js`,
+      },
+      input: {
+        30: resolve(__dirname, root, '30/index.html'),
+        31: resolve(__dirname, root, '31/index.html')
+      }
+    }
   },
 });
